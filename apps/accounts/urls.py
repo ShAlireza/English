52475a0cc3a6e090a7a7eb762c7630faf41eece5
alireza <alirezashateri7@gmail.com>
2020-08-28 10:12:31 +0430

    Add account apiviews url patterns

---
+++ apps/accounts/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'accounts'
+
+urlpatterns = [
+    path('login', view=views.LogoutAPIView.as_view(), name='login'),
+    path('signup', view=views.SignUpAPIView.as_view(), name='signup'),
+    path('logout', view=views.LogoutAPIView.as_view(), name='logout')
+]
